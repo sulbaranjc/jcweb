55b636abeb5daee3de03c21fc7e9c4206e30bee6
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-03-23 16:33:06 +0100

    commit incial del proyecto

---
+++ src/test/java/com/example/jcweb/JcwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
